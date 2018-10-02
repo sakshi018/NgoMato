4b3478e2dbd277e282b0107caba333d0d1dabf2c
Sakshi Agarwal <shaks018@gmail.com>
2018-10-02 19:31:14 +0530

    Donation Modal for requirements and description

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { MatToolbarModule } from '@angular/material';
+import { MatToolbarModule, MatDialogModule } from '@angular/material';
 import { MatSelectModule } from '@angular/material/select';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule, MatInputModule, MatCardModule, MatButtonModule, MatGridListModule, MatIconModule, MatInput, MatTabsModule } from '@angular/material';
@@ -11,7 +11,8 @@ import { NgoListComponent } from './ngo-list/ngo-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgoViewComponent } from './ngo-view/ngo-view.component';
 import { LoginSignupComponent } from './login-signup/login-signup.component';
-
+import { DialogBox } from './ngo-list/ngo-list.component';
+import { MatDialog } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { LoginSignupComponent } from './login-signup/login-signup.component';
     NgoListComponent,
     NgoViewComponent,
     LoginSignupComponent,
+    DialogBox
   ],
   imports: [
     BrowserModule,
@@ -35,9 +37,11 @@ import { LoginSignupComponent } from './login-signup/login-signup.component';
     MatGridListModule,
     HttpClientModule,
     MatIconModule,
-    MatTabsModule
+    MatTabsModule,
+    MatDialogModule
   ],
-  providers: [],
+  providers: [MatDialog],
+  entryComponents: [DialogBox],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
